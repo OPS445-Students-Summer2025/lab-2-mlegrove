70fc94ca8db69e3c7e1e606327b44fa9decc2831
Mark Legrove <mlegrove@myseneca.ca>
2025-05-12 16:21:41 -0400

    Completed Lab 2 with all scripts

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Mark Legrove
+# Author ID: mlegrove
+# Date Created: 2025/05/12
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
